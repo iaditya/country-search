0624756f3ea746c1da304bd947cc217dea2ca926
Aditya Gupta <iadityapansari@gmail.com>
2019-09-24 15:42:52 +0530

    shows results

--- app.js
+++ app.js
@@ -3,14 +3,26 @@ class SearchInput {
     constructor() {
         this.input = document.querySelector('#input-search');
         this.resultBox = document.querySelector('.result-box');
-        this.ul = document.createElement('ul');
-
-
         this.countriesList = [];
         this.countryCodes = [];
+        this.addedTags = [];
+    }
 
+    checkIfAlreadyAdded = (tag) => {
+        return this.addedTags.find(item => item.numericCode === tag.numericCode);
+    }
 
+    addCountry = (country) => {
 
+        if (checkIfAlreadyAdded(tag)) {
+            alert("Alert Added");
+            return;
+        }
+
+        const tag = this.countriesList.find(item => country.numericCode === item.numericCode);
+
+        this.addedTags.push(tag);
+        console.log(this.addedTags);
     }
 }
 
@@ -46,12 +58,14 @@ function init() {
     instance.input.addEventListener('input', (e) => inputChanges(e));
 
     function inputChanges(e) {
+        instance.resultBox.innerHTML = '';
+
         //if finds value
         if (e.target.value.length) {
             instance.resultBox.classList.add('show');
             instance.input.value = (e.target.value).toUpperCase();
             searchCountry(instance.input.value);
-        } 
+        }
         // hide the search box
         else {
             instance.resultBox.classList.remove('show');
@@ -59,17 +73,27 @@ function init() {
     }
 
     function searchCountry(search_term) {
+        let ul = document.createElement('ul');
         console.log(search_term);
         instance.countryCodes
             .filter(country => {
-                console.log(search_term);
+                //console.log(search_term);
                 return country.alpha3Code.toLowerCase().includes(search_term.toLowerCase())
             })
             .forEach(item => {
                 const li = document.createElement('li');
-                //li.classList.add('country-item');
+                li.classList.add('search-item');
+                li.innerText = item.name + ": ";
 
-            })
+                const span = document.createElement('span');
+                span.innerText = item.alpha3Code;
+
+                li.appendChild(span);
+
+                ul.appendChild(li);
+            });
+
+        instance.resultBox.appendChild(ul);
     }
 }
 
