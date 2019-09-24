1ab1c3fe724b3686b3fbfaf5c1b085477104abe4
Aditya Gupta <iadityapansari@gmail.com>
2019-09-24 16:44:02 +0530

    adds, removes country to/from list,focus on input

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ class SearchInput {
     constructor() {
         this.input = document.querySelector('#input-search');
         this.resultBox = document.querySelector('.result-box');
+        this.selectedItemsContainer = document.querySelector('.selected-items');
+        this.completeDataContainer = document.querySelector('.full-data');
         this.countriesList = [];
         this.countryCodes = [];
         this.addedTags = [];
+
+        this.completeDataContainer.innerText = '[]';
     }
 
     checkIfAlreadyAdded = (tag) => {
@@ -14,15 +18,46 @@ class SearchInput {
 
     addCountry = (country) => {
 
-        if (checkIfAlreadyAdded(tag)) {
+        if (this.checkIfAlreadyAdded(country)) {
             alert("Alert Added");
             return;
         }
 
         const tag = this.countriesList.find(item => country.numericCode === item.numericCode);
 
+        const span = document.createElement('span');
+        span.setAttribute('id', tag.numericCode);
+        span.innerText = tag.alpha3Code + " ";
+        span.classList.add('selected-item');
+
+        const removeButton = document.createElement('span');
+        removeButton.innerText = 'x';
+        removeButton.classList.add('close');
+        // span.setAttribute('key', numericCode);
+        removeButton.addEventListener("click", () => this.removeCountry(tag.numericCode));
+
+
+        span.appendChild(removeButton);
+        this.selectedItemsContainer.appendChild(span);
+
         this.addedTags.push(tag);
-        console.log(this.addedTags);
+        this.input.value = '';
+        this.resultBox.classList.remove('show');
+        this.input.focus();
+
+        this.completeDataContainer.innerText = JSON.stringify(this.addedTags);
+    }
+
+    removeCountry = (numericCode) => {
+        this.addedTags = this.addedTags.filter((item) => {
+            return item.numericCode !== numericCode;
+        });
+
+        this.completeDataContainer.innerText = JSON.stringify(this.addedTags);
+
+        const elem = document.getElementById(numericCode);
+        elem.remove();
+        this.input.focus();
     }
 }
 
@@ -57,7 +92,12 @@ function init() {
 
     instance.input.addEventListener('input', (e) => inputChanges(e));
 
+
+
+
+
     function inputChanges(e) {
+        console.log(e.target.value.length);
         instance.resultBox.innerHTML = '';
 
         //if finds value
@@ -74,7 +114,7 @@ function init() {
 
     function searchCountry(search_term) {
         let ul = document.createElement('ul');
-        console.log(search_term);
+
         instance.countryCodes
             .filter(country => {
                 //console.log(search_term);
@@ -89,6 +129,11 @@ function init() {
                 span.innerText = item.alpha3Code;
 
                 li.appendChild(span);
+
+                li.addEventListener("click", function () {
+                    instance.addCountry(item);
+
+                });
 
                 ul.appendChild(li);
             });
